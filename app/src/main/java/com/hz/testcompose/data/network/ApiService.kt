f21581132e7f3edaa5244306e711a089d1c79675
holden.guo <holden.guo@welab.co.id>
2025-03-04 11:02:43 +0800

    feat: update ApiService

--- app/src/main/java/com/hz/testcompose/data/network/ApiService.kt
+++ app/src/main/java/com/hz/testcompose/data/network/ApiService.kt
@@ -2,8 +2,12 @@ package com.hz.testcompose.data.network
 
 import com.hz.testcompose.data.local.Todo
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
     @GET("todos/1")
     suspend fun getTodo(): Todo
+
+    @GET("todos/{id}")
+    suspend fun getTodoById(@Path("id") id: Int): Todo
 }
